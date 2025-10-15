20e5b2b081f7ad18eba81f7681aed79d51b41f62
Victor Blanchard <viblanc@proton.me>
2025-10-15 14:12:14 +0200

    first commit

---
+++ profile-manager-backend/src/main/java/com/github/viblanc/profilemanager/ProfileManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.viblanc.profilemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileManagerBackendApplication.class, args);
+	}
+
+}
---
+++ profile-manager-backend/src/test/java/com/github/viblanc/profilemanager/ProfileManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viblanc.profilemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
