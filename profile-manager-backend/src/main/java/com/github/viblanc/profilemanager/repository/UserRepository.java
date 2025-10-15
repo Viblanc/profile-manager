bc1e10d04ff618d6e40a9385f81d5cb9020f9a22
Victor Blanchard <viblanc@proton.me>
2025-10-15 17:17:28 +0200

    add query to find user by user type

--- profile-manager-backend/src/main/java/com/github/viblanc/profilemanager/repository/UserRepository.java
+++ profile-manager-backend/src/main/java/com/github/viblanc/profilemanager/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.github.viblanc.profilemanager.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -7,4 +9,5 @@ import com.github.viblanc.profilemanager.entity.User;
 
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
+	List<User> findByUserTypeName(String type);
 }
