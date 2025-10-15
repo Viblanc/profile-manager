bfaea03bcdb0bf01c94fb602f24ba6dbdd7c4706
Victor Blanchard <viblanc@proton.me>
2025-10-15 14:38:10 +0200

    create user and user type entities

---
+++ profile-manager-backend/src/main/java/com/github/viblanc/profilemanager/entity/UserType.java
@@ -0,0 +1,50 @@
+package com.github.viblanc.profilemanager.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_type")
+public class UserType {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "user_type", nullable = false)
+	private String type;
+
+	@OneToMany(mappedBy = "type", cascade = CascadeType.ALL)
+	private Set<User> users;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+}
