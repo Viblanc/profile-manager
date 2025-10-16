3be0ee46a6b3fa384553dcc5ba4ece5c447e3b26
Victor Blanchard <viblanc@proton.me>
2025-10-16 10:22:20 +0200

    expose ids of resources

--- profile-manager-backend/src/main/java/com/github/viblanc/profilemanager/config/DataRestConfig.java
+++ profile-manager-backend/src/main/java/com/github/viblanc/profilemanager/config/DataRestConfig.java
@@ -8,8 +8,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import com.github.viblanc.profilemanager.entity.User;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+	private EntityManager entityManager;
+	
+	public DataRestConfig(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		// disable HTTP methods that will change data for the following resources
@@ -19,6 +28,9 @@ public class DataRestConfig implements RepositoryRestConfigurer {
 		for (Class<?> clazz : resources) {
 			disableHttpMethodsForResource(clazz, config, unsupportedMethods);
 		}
+		
+		// expose ids of resources
+		exposeIds(config);
 	}
 
 	private void disableHttpMethodsForResource(Class<?> clazz, RepositoryRestConfiguration config,
@@ -26,5 +38,14 @@ public class DataRestConfig implements RepositoryRestConfigurer {
 		config.getExposureConfiguration().forDomainType(clazz)
 				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(disabledHttpMethods))
 				.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(disabledHttpMethods));
+	}
+	
+	private void exposeIds(RepositoryRestConfiguration config) {
+		// gather entity types in an array
+		Class<?>[] entityTypes = entityManager.getMetamodel().getEntities().stream().map(EntityType::getJavaType)
+				.toArray(Class[]::new);
+
+		// expose entity ids
+		config.exposeIdsFor(entityTypes);
 	}
 }
