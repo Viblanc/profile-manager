b718435b219a21dc73b5820e3a2075667e5bc311
Victor Blanchard <viblanc@proton.me>
2025-10-17 13:37:04 +0200

    dockerize application

--- profile-manager-backend/src/main/java/com/github/viblanc/profilemanager/ProfileManagerBackendApplication.java
+++ profile-manager-backend/src/main/java/com/github/viblanc/profilemanager/ProfileManagerBackendApplication.java
@@ -1,13 +1,28 @@
 package com.github.viblanc.profilemanager;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProfileManagerBackendApplication {
+	@Value("${allowed.origins}")
+	private String[] allowedOrigins;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileManagerBackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedMethods("*").allowedHeaders("*").allowedOrigins(allowedOrigins);
+			}
+		};
+	}
 }
